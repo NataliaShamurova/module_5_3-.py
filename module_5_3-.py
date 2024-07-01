4b3980e906db4dc0526a6c953efe02242ae06940
NataliaShamurova <shamurovann@yandex.ru>
2024-07-01 08:19:13 +0300

    Add files via upload

---
+++ module_5_3-.py
@@ -0,0 +1,30 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        houses_history = super().__new__(cls)
+        cls.houses_history.append(args[0])
+
+        return houses_history
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        #self.number_of_floors = number_of_floors  # кол-во этажей
+
+    def __del__(self):
+      print(f'{self.name} снесён, но он останется в истории')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+#
+# Удаление объектов
+del h2
+del h3
+#
+print(House.houses_history)
+
